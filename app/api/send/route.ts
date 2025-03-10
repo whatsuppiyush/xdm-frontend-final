bb1963efff43867df811480f73d185dc267dd0f6
vijit2k7 <vijit2k7@yahoo.in>
2025-03-10 23:37:22 +0530

    fixing env file changes

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,7 +1,7 @@
 import { EmailTemplate } from '@/components/EmailTemplate';
 import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY || '');
 
 export async function POST(request: Request) {
   try {
