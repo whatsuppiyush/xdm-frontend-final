3b4a0cfdb697b99f1d7c76cb908a352c66b45cef
amplifyxlabs <amplifyxlabstech@gmail.com>
2025-03-22 17:15:06 +0800

    Fixing 450 dm /day logic

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,5 +1,5 @@
 // Message rate limits
-export const DAILY_MESSAGE_LIMIT = 5;
+export const DAILY_MESSAGE_LIMIT = 450;
 
 // Other constants can be added here in the future
 export const MESSAGE_DELAY_MINUTES = 2; // Delay between messages in minutes 
